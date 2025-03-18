e628487fec2fe61029f00e1d77a48d37ba0df07e
IsakOswald <isak.oswald.cs@gmail.com>
2025-03-18 16:04:58 +1100

    Uploading basic C++/C# projects

---
+++ C#/Basic Skills/BasicGUI.cs
@@ -0,0 +1,65 @@
+﻿using SplashKitSDK;
+using System.Diagnostics;
+using static SplashKitSDK.SplashKit;
+using static System.Convert;
+
+const int WINX = 800, WINY = 600;
+int radius = 50;
+
+
+
+OpenWindow("TYK2", WINX, WINY);
+ClearScreen();
+RefreshScreen();
+
+
+
+
+while (!QuitRequested())
+{
+    
+
+    
+    if(KeyDown(KeyCode.CKey))
+    {
+        ClearScreen(RandomColor());
+        RefreshScreen(7);
+    }
+
+
+    if(KeyDown(KeyCode.SKey))
+    {
+        radius = 10;
+    }
+    
+    if(KeyDown(KeyCode.MKey))
+    {
+        radius = 50;
+    }
+
+    if(KeyDown(KeyCode.LKey))
+    {
+        radius = 100;
+    }
+
+    if(MouseDown(MouseButton.LeftButton))
+    {
+        FillCircle(RandomColor(), MouseX(),MouseY(),radius);
+        RefreshScreen(6);
+    }
+
+
+
+
+
+
+    
+    
+
+
+    ProcessEvents();
+}
+
+
+
+
---
+++ C#/Basic Skills/BasicStats.cs
@@ -0,0 +1,82 @@
+﻿using static SplashKitSDK.SplashKit;
+using static System.Convert;
+
+
+//Create varibles in global scope.
+double total = 0, min = 0, max = 0, average = 0;
+int count = 0;
+string answer, line;
+
+
+
+
+
+
+do
+{
+    //Create a local varible in scope.
+    double value;
+    
+    //Prompt for a value
+    Write("Enter a value: ");
+    line = ReadLine();
+
+    //Handle Errors
+    while (! IsDouble(line))
+    {
+        Write("Enter a valid value: ");
+        line = ReadLine();
+    }
+
+    value = ToDouble(line);
+
+   
+    
+    //Increase Count
+    count ++;
+    WriteLine($"Count: {count}");
+
+    //Increase total with the current value
+    total += value;
+    WriteLine($"Total: {total}");
+
+    //Check min
+    if (value < min || count == 1)
+    {
+        min = value;
+    }
+    WriteLine($"Min: {min}");
+
+    //Check max
+    if (count == 1 || value > max)
+    {
+        max = value;
+    }
+    WriteLine($"Max: {max}");
+  
+
+    //Calculate avg
+    average = total / count;
+    WriteLine($"Average: {average}");
+
+    WriteLine();
+
+    Write("Go again? [y/n]: ");
+    answer = ReadLine();
+
+    //Handle exiting.
+    while((answer != "n" && answer != "N") && (answer != "y" && answer != "Y"))
+    {
+        Write("Not a valid entry. Go again? [y/n]:");
+        answer = ReadLine();
+    }
+
+
+    
+
+
+    
+
+
+}while(answer != "n" && answer != "N");
+
